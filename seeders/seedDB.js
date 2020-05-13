32fb366409605bc2f11ee6d060b34109958a14bb
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-12 20:06:21 -0500

    created service worker and manifest

---
+++ seeders/seedDB.js
@@ -0,0 +1,35 @@
+var mongoose = require("mongoose");
+var db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+});
+
+var budgetSeed = [
+  {
+    name: "Income",
+    value: 5000,
+    date: 05 / 02 / 2020,
+  },
+  {
+    name: "Mortgage",
+    value: 1000,
+    date: 05 / 06 / 2020,
+  },
+  {
+    name: "Groceries",
+    value: 200,
+    date: 05 / 05 / 2020,
+  },
+];
+
+db.Transaction.deleteMany({})
+  .then(() => db.Transaction.collection.insertMany(budgetSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
